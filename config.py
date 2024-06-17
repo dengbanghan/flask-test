6fa2024a623b9e112ed24958d81cf9d331650bca
dengbanghan <dengbanghan@gmail.com>
2024-06-17 23:12:13 +0800

    1. 修改SECRET_KEY的值； 2. 将MySQL地址的IP替换成域名。

--- config.py
+++ config.py
@@ -6,11 +6,11 @@
 # @Software: PyCharm
 
 class BaseConfig:
-    SECRET_KEY ="your-secret-key"
+    SECRET_KEY ="dengbanghan"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:abcd9876@47.119.20.187:3306/users?charset=utf8mb4"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:abcd9876@server.dengbanghan.top:3306/users?charset=utf8mb4"
 
 class QaConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = "mysql+pymysgl://[测试服务器MySQL 用户名]:[测试服务器 MySQL 密码]@[测试服务器 MySQL 域名]:[测试服务器 MySQL 端口号]/pythonbbs?charset=utf8mb4n"
